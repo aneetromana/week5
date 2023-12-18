a1abc1b96e777c71df252f9d3d8a18c2a3cc9526
Aneet Romana <55719972+aneetromana@users.noreply.github.com>
2023-12-17 20:01:36 -0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import TaskList from "./components/TaskList";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>To Do App</h1>
+      <TaskList />
+    </div>
+  );
+}
